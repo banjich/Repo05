cb00dfba31056de700fba9dd83ccf6104d25025c
banjich <banjich@gmail.com>
2021-09-18 10:38:46 +0200

    Added file index.js

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    wordBlanks = [myAdjective + " white " + myNoun +" " + myVerb +" "+ myAdverb + "."]
+    return wordBlanks;
+    
+    }
+
+console.log(main());
+module.exports = main;
